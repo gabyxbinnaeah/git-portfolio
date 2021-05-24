0c6be0bf84db88fb423173f237e6868c61774ebf
gabriel odhiambo <gabyxbinnaeh4@gmail.com>
2021-05-24 23:40:21 +0300

    adds directive to highlight projrct title onmouseover with darkgray color

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,10 +1,25 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Host, HostListener} from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
+  
 
-  constructor() { }
+  constructor(private elem:ElementRef) { }
+  @HostListener("mouseover") onmouseover(){
+    this.textHighlight("darkgray");
+  }
+
+  @HostListener("mouseout") onmouseout(){
+    this.textHighlight("");
+  }
+
+
+      private textHighlight(action:string){
+        this.elem.nativeElement.style.backgroundColor =action; //elementRef grant us direct access to the host DOM element through its nativeElement prperty
+  
+      }
+
 
 }
