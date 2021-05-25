42046e98ea6498517c3ad0f001f5230164029062
gabriel odhiambo <gabyxbinnaeh4@gmail.com>
2021-05-25 06:37:51 +0300

    adds the descriptive words in about section for the app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
    { path: 'users', component:LandingPageComponent },
   { path: 'About', component:AboutComponent },
+  { path: '', redirectTo:"/goals", pathMatch:"full"},
+  { path:'**', component:NotFoundComponent},
 
 ];
 
